941a179bd3f1a98fdc11dc256b54d733fece938b
Tomás Salina <salinatomass53@gmail.com>
2020-10-19 21:30:56 -0300

    Features: Play and Pause

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+const video = document.querySelector('video')
+const button = document.querySelector('button')
+
+function MediaPlayer(config) {
+  this.media = config.el
+}
+
+MediaPlayer.prototype.play = function() {
+  this.media.play()
+}
+
+MediaPlayer.prototype.pause = function() {
+  this.media.pause()
+}
+
+MediaPlayer.prototype.togglePlay = function() {
+  (this.media.paused) 
+    ? this.play()
+    : this.pause()
+}
+
+const player = new MediaPlayer( { el: video } )
+button.onclick = () => player.togglePlay()
